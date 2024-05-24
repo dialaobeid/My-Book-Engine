bc78bbbf614217399b2af7c8222934ef27e5717f
dobeid <dialajo9@gmail.com>
2024-05-23 20:00:48 -0700

    middleware fixes

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startApolloServer() {
+  // Create Apollo server instance
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => authMiddleware({ req }),  
+  });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Start the Apollo server
+  await server.start();
+  console.log('Apollo Server started.');
+
+  // Apollo GraphQL middleware 
+  server.applyMiddleware({ app });
+  console.log('Apollo Middleware applied.');
+
+  // Middleware
+  app.use(cors());  // Enable CORS
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // Connects to db & starts the Express server
+  db.once('open', () => {
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Start the server
+startApolloServer();
